5167a091bbb34c0ca542789d9eae38660a0813a3
spsanderson <spsanderson@gmail.com>
2018-08-27 21:56:04 -0400

    create time-series objecta, time aware tibble and recreate time index from data.

--- ts_cci30_analysis.R
+++ ts_cci30_analysis.R
@@ -1,4 +1,4 @@
-# Load Libs #########################################################
+# Load Libs ####
 # Time Series analysis of CCI30 crypto index
 library(tidyquant)
 library(timetk)
@@ -6,9 +6,22 @@ library(tibbletime)
 library(sweep)
 library(anomalize)
 
-# Daily OHLCV ######################################################
+# Daily OHLCV ####
 url <- "https://cci30.com/ajax/getIndexHistory.php"
 destfile <- "cci30_OHLCV.csv"
 download.file(url, destfile = destfile)
 df <- read.csv("cci30_OHLCV.csv")
 rm(list = c('url','destfile'))
+
+# Create TS ####
+df$Date <- lubridate::ymd(df$Date)
+df_ts <- tk_ts(df, start = 2015, frequency = 365)
+head(df_ts)
+str(df_ts)
+# create time aware tibble
+df_tibble <- as_tbl_time(df, index = Date)
+df_tibble
+
+# Extract original index in the date format using tk_index
+timetk_index <- tk_index(df_ts, timetk_idx = TRUE)
+head(timetk_index)

03f3823c3ca5cb1180eb9c5beddc28d91bf8bd14
spsanderson <spsanderson@gmail.com>
2018-08-27 22:21:36 -0400

    graph test and training regions of data

--- ts_cci30_analysis.R
+++ ts_cci30_analysis.R
@@ -18,10 +18,73 @@ df$Date <- lubridate::ymd(df$Date)
 df_ts <- tk_ts(df, start = 2015, frequency = 365)
 head(df_ts)
 str(df_ts)
-# create time aware tibble
-df_tibble <- as_tbl_time(df, index = Date)
-df_tibble
+
+# Create time aware tibble
+df_tibble_ts <- as_tbl_time(df, index = Date)
+df_tibble_ts
 
 # Extract original index in the date format using tk_index
 timetk_index <- tk_index(df_ts, timetk_idx = TRUE)
 head(timetk_index)
+class(timetk_index)
+
+# We can now get the original date index using the 
+# tk_tbl() argument timetk_idx = TRUE
+df_tibble <- df_tibble_ts %>%
+  tk_tbl(index_rename = Date, timetk_idx = T)
+
+df_tibble
+
+plot.ts(df_tibble)
+
+# Take a look at data
+max.value <- max(df_tibble$Close)
+min.value <- min(df_tibble$Close)
+start.date <- min(df_tibble$index)
+end.date <- max(df_tibble$index)
+training.region <- round(nrow(df_tibble) * 0.7, 0)
+test.region <- nrow(df_tibble) - training.region
+training.stop.date <- as.Date(max(df_tibble$index)) %m-% days(
+  as.numeric(test.region))
+
+df_tibble %>%
+  ggplot(
+    aes(
+      x = index
+      , y = Close
+    )
+  ) +
+  geom_rect(
+    xmin = as.numeric(ymd(training.stop.date))
+    , xmax = as.numeric(ymd(end.date))
+    , ymin = (min.value * 0.9)
+    , ymax = (max.value * 1.1)
+    , fill = palette_light()[[4]]
+    , alpha = 0.01
+  ) +
+  annotate(
+    "text"
+    , x = ymd("2016-01-01")
+    , y = 5000
+    , color = palette_light()[[1]]
+    , label = "Training Region"
+  ) +
+  annotate(
+    "text"
+    , x = ymd("2018-01-01")
+    , y = 1200
+    , color = palette_light()[[1]]
+    , label = "Testing Region"
+  ) +
+  geom_point(
+    alpha = 0.5
+    , color = palette_light()[[1]]
+  ) +
+  labs(
+    title = "Discharges: Monthly Scale"
+    , subtitle = "Source: DSS"
+    , caption = "Based on discharges from 2010-01-01 through 2018-07-31"
+    , y = "Count"
+    , x = ""
+  ) +
+  theme_tq()

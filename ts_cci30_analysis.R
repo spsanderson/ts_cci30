41f625ffab3d6f48f7f97e6c65d6b9b534f294b7
spsanderson <spsanderson@gmail.com>
2018-08-27 20:55:17 -0400

    initial commit.

---
+++ ts_cci30_analysis.R
@@ -0,0 +1,9 @@
+# Load Libs #########################################################
+# Time Series analysis of CCI30 crypto index
+library(tidyquant)
+library(timetk)
+library(tibbletime)
+library(sweep)
+library(anomalize)
+
+# Daily OHLC #######################################################

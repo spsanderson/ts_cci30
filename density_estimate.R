eac10f5159344a5a36f4b2dd1cb4a8c0a254e3b7
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-11 21:42:15 -0500

    Update density_estimate.R

--- density_estimate.R
+++ density_estimate.R
@@ -8,46 +8,34 @@ pacman::p_load(
   "tidyquant"
 )
 
-source("00_scripts/data_load.R")
+getSymbols('ETH-USD') # Get daily price data from yahooFinance
+eth <- `ETH-USD` # change the name of the data
+eth <- eth[,4] %>% Delt(k=1) %>% na.omit() # pull adjusted close, convert to return, and remove NA data
 
-cci_index_tbl <- read_csv("00_data/cci30_OHLCV.csv") %>%
-  clean_names()
+# time_param <- "weekly"
+# log_returns_tbl <- cci_index_tbl %>%
+#   tq_transmute(
+#     select = close
+#     , mutate_fun = periodReturn
+#     , period = time_param
+#     , type = "log"
+#     , col_rename = "value"
+#   ) %>%
+#   set_names("date_col", "value")
 
-time_param <- "weekly"
-log_returns_tbl <- cci_index_tbl %>%
-  tq_transmute(
-    select = close
-    , mutate_fun = periodReturn
-    , period = time_param
-    , type = "log"
-    , col_rename = "value"
-  ) %>%
-  set_names("date_col", "value")
+pars_init <- eth %>% coredata() %>% stable_fit_init()
+pars_as <- eth %>% coredata() %>% stable_fit_koutrouvelis(pars_init = pars_init)
+pars_gauss <- c(mean(coredata(eth)), sd(coredata(eth)))
+pars_logistic <- eth %>% coredata() %>% as.numeric() %>% elogis()
 
-pars_init <- log_returns_tbl %>%
-  pull(value) %>%
-  coredata() %>%
-  stable_fit_init()
-
-pars_as <- log_returns_tbl %>%
-  pull(value) %>%
-  coredata() %>%
-  stable_fit_koutrouvelis(pars_init = pars_init)
-
-pars_gauss <- c(mean(coredata(log_returns_tbl$value)), sd(coredata(log_returns_tbl$value)))
-pars_logistic <- log_returns_tbl$value %>% 
-  coredata() %>% 
-  as.numeric() %>% 
-  elogis()
-
-N = nrow(log_returns_tbl)
+N = nrow(eth)
 rnd_as <- stable_rnd(N, pars = pars_as)
 rnd_gauss <- rnorm(N, mean = pars_gauss[1], sd = pars_gauss[2])
 rnd_logistic <- rlogis(N, location = pars_logistic$parameters[1],
                        scale = pars_logistic$parameters[2])
 
 data_tbl <- tibble(
-  CCI_Empirical = log_returns_tbl$value
+  ETH_Empirical = eth$Delt.1.arithmetic %>% as.numeric()
   , Random_Alpha_Stable = rnd_as
   , Random_Gaussian = rnd_gauss
   , Random_Logistic = rnd_logistic
@@ -71,7 +59,7 @@ p1 <- data_tbl %>%
   theme(legend.position = "bottom")
 
 p2 <- data_tbl %>%
-  filter(name %in% c("CCI_Empirical","Random_Alpha_Stable")) %>%
+  filter(name %in% c("ETH_Empirical","Random_Alpha_Stable")) %>%
   ggplot(aes(x = value, fill = as.factor(name))) +
   geom_density(alpha = .2) +
   theme_minimal() +
@@ -79,7 +67,7 @@ p2 <- data_tbl %>%
   labs(
     x = "Log Returns"
     , y = "Density Estimate"
-    , title = "CCI30 Weekly Empirical Log Returns"
+    , title = "ETH Weekly Empirical Log Returns"
     , fill = ""
   ) +
   theme(legend.position = "bottom")
